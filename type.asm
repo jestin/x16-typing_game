c7c74fbe6ef0ea52e997003751c0f920b98e3cf6
Jestin Stoffel <jestin.stoffel@gmail.com>
2019-12-30 22:52:41 -0800

    Refactor write_char into a macro

--- type.asm
+++ type.asm
@@ -13,9 +13,7 @@ zp_color_addr =$00
 	ldx #0
 -	lda .addr,x
 	beq +
-	sta veradat
-	lda zp_color_addr
-	sta veradat
+	+write_char
 	inx
 	jmp -
 
@@ -23,6 +21,12 @@ zp_color_addr =$00
 +	pla
 	tax
 	pla
+}
+
+!macro write_char {
+	sta veradat
+	lda zp_color_addr 		;foreground/background color
+	sta veradat
 }
 
 main:
@@ -56,31 +60,6 @@ init:
 	rts
 
 ;------------------------------------------------------------------------------
-; write_char
-;
-; This expects a color and a character pushed to the stack, and will write that
-; character with that color to the current position of veradat
-;------------------------------------------------------------------------------
-write_char:
-	; push the return address to the zero page
-	pla			;low byte of return
-	sta $0
-	pla			;high byte of return
-	sta $1
-
-	pla			;character
-	sta veradat
-	pla 		;foreground/background color
-	sta veradat
-
-	; pop the return address from the zero page
-	lda $1
-	pha
-	lda $0
-	pha
-	rts
-
-;------------------------------------------------------------------------------
 ; set_vera_xy
 ;
 ; This expects the X and Y registers to be set to the desired XY postition, and
@@ -101,14 +80,13 @@ set_vera_xy:
 clear_screen:
 	pha			;push accumulator to the stack
 
+	lda #$00 		;color
+	sta zp_color_addr
 	ldx #0
 	ldy #0
 -	jsr set_vera_xy
-	lda #$00 		;color
-	pha
 	lda #$20		;character
-	pha
-	jsr write_char
+	+write_char
 	cpx #79
 	bne +			; on 79, increment Y and reset X
 	cpy #59
