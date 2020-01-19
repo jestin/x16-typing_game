12c11ddad3f2a6fa1c103224f492c098ef28148e
Jestin Stoffel <jestin.stoffel@gmail.com>
2020-01-19 13:37:14 -0800

    Draw border

--- type.asm
+++ type.asm
@@ -13,68 +13,61 @@ main:
 	+LoadW r0, 0
 	jsr GRAPH_init
 
-	lda #1
-	ldx #0
-	ldy #0
-	jsr GRAPH_set_colors
+	; lda #1
+	; ldx #0
+	; ldy #0
+	; jsr GRAPH_set_colors
 	jsr GRAPH_clear
 
 	jsr draw_border
-	jmp end
-
-	; draw full pallet
-	+LoadW r0, 0
-	+LoadW r1, 0
-	+LoadW r2, 0
-	+LoadW r3, 198
-
--	lda r0L
-	clc
-	adc #110
-	jsr GRAPH_set_colors
-	jsr GRAPH_draw_line
-	inc r0L
-	inc r2L
-	bne -
 
 	jmp end
 
 draw_border:
-	; left
+	lda #0
+	jsr GRAPH_set_colors
+
+	; outer rect
+	; To make the outer rectangle 2 pixels wide, we draw two rectanlges
 	+LoadW r0, 0
 	+LoadW r1, 0
-	+LoadW r2, 0
+	+LoadW r2, 320
 	+LoadW r3, 200
-	
--	lda r0L
-	clc
-	adc #110
-	jsr GRAPH_set_colors
-	jsr GRAPH_draw_line
-	inc r0L
-	inc r2L
-	lda r0L
-	cmp #7
-	bne -
+	+LoadW r4, 0
+	jsr GRAPH_draw_rect
+	+LoadW r0, 1
+	+LoadW r1, 1
+	+LoadW r2, 318
+	+LoadW r3, 198
+	clc						; no reason to fill this rect
+	jsr GRAPH_draw_rect
 
-	; right
-	+LoadW r0, 312
-	+LoadW r1, 0
-	+LoadW r2, 312
-	+LoadW r3, 200
-	
--	lda r0L
-	clc
-	adc #110
-	jsr GRAPH_set_colors
-	jsr GRAPH_draw_line
-	inc r0L
-	inc r2L
-	lda r0L
-	cmp #7
-	bne -
+	; middle rect (fill)
+	+LoadB r4L, 0 			; loop counter
+	+LoadB r4H, 108			; color
 
-	rts
+-	lda r4H
+	jsr GRAPH_set_colors
+	inc r0L
+	inc r1L
+	dec r2L
+	dec r2L
+	dec r3L
+	dec r3L
+	; +LoadW r0, 2
+	; +LoadW r1, 2
+	; +LoadW r2, 317
+	; +LoadW r3, 197
+	clc						; no reason to fill this rect
+	jsr GRAPH_draw_rect
+	inc r4L
+	dec r4H
+	lda r4L
+	cmp #7
+	beq +
+	jmp -
+
++	rts
 
 end:
 	jmp *
