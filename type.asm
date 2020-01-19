061aacfb3af49dea305f1c430744f53786cc034f
Jestin Stoffel <jestin.stoffel@gmail.com>
2020-01-19 11:29:51 -0800

    Remove dead code

--- type.asm
+++ type.asm
@@ -76,65 +76,6 @@ draw_border:
 
 	rts
 
-
-init:
-	; init vera
-	lda #0
-	sta veractl
-	sta veralo
-	sta veramid
-	lda #$10		;set increment to 1, high address to 0
-	sta verahi
-
-	; init zero page
-	lda #0
-	sta zp_color_addr
-	sta zp_skip_addr
-	lda #$ff
-	sta zp_take_addr
-	rts
-
-;------------------------------------------------------------------------------
-; set_vera_xy
-;
-; This expects the X and Y registers to be set to the desired XY postition, and
-; then sets veralo, veramid, and verahi accordinglingly
-;------------------------------------------------------------------------------
-set_vera_xy:
-	pha			;push accumulator to the stack
-
-	tya			;transfer y to the accumulator
-	sta veramid
-	txa			;pop x from stack
-	asl
-	sta veralo	;store in vera low byte address
-
-	pla			;pop accumulator from the stack
-	rts
-
-clear_screen:
-	pha			;push accumulator to the stack
-
-	lda #$00 		;color
-	sta zp_color_addr
-	ldx #0
-	ldy #0
--	jsr set_vera_xy
-	lda #$20		;character
-	+write_char
-	cpx #79
-	bne +			; on 79, increment Y and reset X
-	cpy #59
-	beq ret
-	iny
-	ldx #0
-	jmp -
-+	inx
-	jmp -
-ret:
-	pla			;pop accumulator from the stack
-	rts
-
 end:
 	jmp *
 	rts
