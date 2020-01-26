6ef5b777418de7d5f3306f07570828b43c76bfe2
Jestin Stoffel <jestin.stoffel@gmail.com>
2020-01-25 22:39:38 -0800

    Create AddW and SubW macros

--- macros.asm
+++ macros.asm
@@ -23,31 +23,24 @@ MACROS = 1
 	+MoveB .source+1, .dest+1
 }
 
-!macro write_string .addr {
-	; push x, y, and a to the stack
-	pha
-	txa
-	pha
-	tya
-	pha
+!macro AddW .dest, .value {
+	lda .dest
+	clc
+	adc #<(.value)
+	sta .dest
+	lda .dest+1
+	adc #>(.value)
+	sta .dest+1
+}
 
-	ldx zp_skip_addr
-	ldy zp_take_addr
--	lda .addr,x
-	beq +
-	cpy #0
-	beq +
-	+write_char
-	inx
-	dey
-	jmp -
-
-	; restore x, y, and a
-+	pla
-	tay
-	pla
-	tax
-	pla
+!macro SubW .dest, .value {
+	lda .dest
+	sec
+	sbc #<(.value)
+	sta .dest
+	lda .dest+1
+	sbc #>(.value)
+	sta .dest+1
 }
 
 !macro write_char {
